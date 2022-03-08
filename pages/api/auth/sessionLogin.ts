b6498b83aeb6b92d6cffef883d42c915823ac95c
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-03-08 11:53:50 +0800

    auth api

---
+++ pages/api/auth/sessionLogin.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { setCookie } from 'nookies';
+
+import { auth } from 'utils/FirebaseServer';
+
+const SESSION_EXPIRATION = 1000 * 60 * 60 * 24 * 7; // 1 week
+const SESSION_COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'lax',
+  maxAge: SESSION_EXPIRATION,
+  path: '/',
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const idToken = req.body.idToken;
+  if (typeof idToken !== 'string') {
+    res.status(400).end();
+    return;
+  }
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_EXPIRATION,
+  });
+  setCookie({ res }, 'session', sessionCookie, SESSION_COOKIE_OPTIONS);
+
+  const { uid } = await auth.verifyIdToken(idToken);
+
+  res.status(200).end();
+}
