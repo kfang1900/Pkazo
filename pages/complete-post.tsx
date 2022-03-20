c4adb5049f44da95f1d647346faf12dd3eddb3d2
frodakcin <huangsy222@gmail.com>
2022-03-20 14:39:56 -0400

    Import issues

--- pages/complete-post.tsx
+++ pages/complete-post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/uploading/Header';
+import Header from '../components/Header';
 import CompleteWorkPortfolio from '../components/uploading/CompleteWorkPortfolio';
 import CompleteWorkTabSelector from '../components/uploading/CompleteWorkTabSelector.js';
 
