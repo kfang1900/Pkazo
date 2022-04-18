893b1b00db5243b0effb08553c046c4ce35fd6fa
efraimdahl <efraimdahl@uchicago.edu>
2022-04-17 22:39:45 -0500

    reading images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
-  }
+  },
+  images: { domains: ['firebasestorage.googleapis.com'], }
 };
 
 module.exports = nextConfig;
