668caeeb141782be7585690449a5135faaf80f0b
efraimdahl <efraimdahl@uchicago.edu>
2022-05-01 16:14:10 -0500

    images uploading properly from work upload page

--- components/uploading/CompleteWorkUploadForm.js
+++ components/uploading/CompleteWorkUploadForm.js
@@ -4,9 +4,9 @@ import Header from '../Header.tsx';
 import CompleteWorkTabSelector from './CompleteWorkTabSelector';
 import CompleteWorkPosts from './CompleteWorkPosts';
 import CompleteWorkPortfolio from './CompleteWorkPortfolio';
-import { doc, getFirestore, setDoc,addDoc,collection, } from 'firebase/firestore';
+import { doc, getFirestore, setDoc, addDoc, collection, } from 'firebase/firestore';
 import { getApp } from 'firebase/app';
-import {getStorage,ref,uploadBytesResumable} from 'firebase/storage';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 
 const getBlobFromUri = async (uri) => {
@@ -25,60 +25,54 @@ const getBlobFromUri = async (uri) => {
   return blob;
 };
 
+const uploadImages = async (images) => {
+  const storage = getStorage()
+  const uploadLocation = "Works/" + "YDx1IlGcogMongCBzEo0" + "/"
+  //Then add images to storage
+  let firstRet = null;
+  const promises = await images.map(async (pic) => {
+    const picblob = await getBlobFromUri(pic)
+    console.log(picblob)
+    const storageRef = ref(storage, uploadLocation + picblob.name);
+    await uploadBytesResumable(storageRef, picblob);
+    // Listen for state changes, errors, and completion of the upload.
+    console.log(storageRef, storageRef.fullPath)
+    let path = "gs://" + storageRef.bucket + "/" + storageRef.fullPath
+    if (firstRet === null) {
+      firstRet = path
+    }
+    return path
+  })
+  const imagerefs = await Promise.all(promises)
+  console.log('returning', imagerefs, imagerefs[0])
+  return imagerefs
+}
 
 function CompleteWorkUploadForm() {
   const [stage, setStage] = useState(0);
   const [data, setData] = useState({})
-  const [uploading,setUploading] = useState(false)
+  const [uploading, setUploading] = useState(false)
 
-  const uploadData=async(s)=>{
+  const uploadData = async (s) => {
     setUploading(true)
     //First Create Document with Initial Data
-    console.log("uploading",s)
+    console.log("uploading", s)
     const app = getApp();
     const db = getFirestore(app);
-    const docRef = await setDoc(doc(db, "Works","YDx1IlGcogMongCBzEo0"),s )
-    let imagerefs = []
-    const storage = getStorage()
-    const uploadLocation="Works/"+"YDx1IlGcogMongCBzEo0"+"/"
-    //Then add images to storage
-    let i = 0
-    s.Images.forEach(async(pic) =>{
-      const picblob = await getBlobFromUri(pic)
-      console.log(picblob)
-      const storageRef = ref(storage, uploadLocation + picblob.name);
-      const uploadTask = uploadBytesResumable(storageRef, picblob);
-      // Listen for state changes, errors, and completion of the upload.
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-        },
-        (error) => {
-          setUploading(false);
-          console.log("an error occured during uploading")
-          },
-        async () => {
-          console.log("uploaded image ",pic)
-        }
-      );
-      console.log(storageRef,storageRef.fullPath)
-      let imageRefData = storageRef.fullPath
-      imagerefs.push(imageRefData)
-      i=i+1
-    })
+    const docRef = await setDoc(doc(db, "Works", "YDx1IlGcogMongCBzEo0"), s)
+    const imagerefs = await uploadImages(s.Images)
     //Update the newly created work document with the image references in storage.
-    const mainImage = imagerefs[0]
-    console.log("MainImageRef,",mainImage)
+    console.log("MainImageRef,", imagerefs[0])
     const updateRef = doc(db, 'Works', "YDx1IlGcogMongCBzEo0");
-    setDoc(updateRef, {"MainImage": mainImage,"Images":imagerefs},{ merge: true });
+    setDoc(updateRef, { "MainImage": imagerefs[0], "Images": imagerefs }, { merge: true });
     console.log("finished uploading")
     setUploading(false);
-      }
-  
-  const getData=()=>{
+  }
+
+  const getData = () => {
     return data
   }
-  const functions={setStage,setData,uploadData,getData}
+  const functions = { setStage, setData, uploadData, getData }
 
   return (
     <>
