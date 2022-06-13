a9de33ec2c3a4ae4a6c6e70c52a8523cf5882c51
wolfy26 <43702412+wolfy26@users.noreply.github.com>
2022-06-13 03:16:20 -0400

    test 2

--- components/profile/ProfilePortfolio.tsx
+++ components/profile/ProfilePortfolio.tsx
@@ -104,7 +104,7 @@ function GallerySection({ portfolioData }: { portfolioData: PortfolioObject }) {
       {/* Circle Images Section --End-- */}
       {/* Gallery Section --Start-- */}
       <section tw='mt-3 md:mt-8'>
-        <div className="container" tw='hidden'>
+        <div className="container">
           <Masonry
             breakpointCols={{ default: 3, 500: 2 }}
             className={styles['masonry']}
