97ff463b5ae5f47c9c0394ff8206ffdf0876cb63
Jeffrey Meng <jeffkmeng@gmail.com>
2022-06-26 04:27:14 -0700

    integrate chat

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,12 +1,13 @@
 import useAuth from '../../utils/auth/useAuth';
 import 'twin.macro';
-import { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   getDatabase,
   onChildAdded,
   onValue,
   push,
   ref,
+  serverTimestamp,
 } from '@firebase/database';
 import { getApp } from 'firebase/app';
 import tw from 'twin.macro';
@@ -17,45 +18,35 @@ import {
   query,
   where,
 } from 'firebase/firestore';
+import Image from 'next/image';
+import { timeElapsed } from '../popups/ShowComment';
+import { loadStorageImage } from '../../helpers/FirebaseFunctions';
+import { ArtistData } from '../../types/dbTypes';
+import { ChatMessage, parseDate } from '../../pages/chat';
 
 export default function Chat({
   partnerId,
+  pfp,
+  name,
+  scrollRef,
+  messageRef,
+  messages,
 }: {
   /**
    * userId of the person who the user is chatting with
    */
   partnerId: string;
+  pfp: string;
+  name: string;
+  scrollRef: React.Ref<HTMLDivElement>;
+  messageRef: React.Ref<HTMLInputElement>;
+  messages: ChatMessage[];
 }) {
   const { user } = useAuth();
-  const [messages, setMessages] = useState<
-    {
-      author: string;
-      text: string;
-      version: 1;
-      id: string;
-    }[]
-  >([]);
   const [text, setText] = useState('');
 
   useEffect(() => {
     if (!user) return;
-    const app = getApp();
-    const rtdb = getDatabase(app);
-    const messagesRef = ref(
-      rtdb,
-      'chats/' + [user.uid, partnerId].sort().join('--') + '/messages'
-    );
-    setMessages([]);
-    onChildAdded(messagesRef, (snapshot) => {
-      const data = snapshot.val();
-      setMessages((v) => [
-        ...v,
-        {
-          ...data,
-          id: snapshot.key,
-        },
-      ]);
-    });
   }, [user, partnerId]);
   const onSend = useCallback(() => {
     if (!user) return;
@@ -70,63 +61,113 @@ export default function Chat({
       author: user.uid,
       version: 1,
       text: text,
+      timestamp: serverTimestamp(),
     });
     setText('');
   }, [user, text, setText, partnerId]);
-  const [partnerData, setPartnerData] = useState<
-    | {
-        name: string;
-      }
-    | Record<string, any>
-  >({});
-  useEffect(() => {
-    (async () => {
-      const app = getApp();
-      const db = getFirestore(app);
-      const artistsRef = collection(db, 'artists');
-      const q = query(artistsRef, where('associatedUser', '==', partnerId));
 
-      const ref = await getDocs(q);
-
-      ref.forEach((snapshot) => {
-        // this assumes that there will only be one result
-        setPartnerData({
-          name: snapshot.data().name,
-        });
-      });
-    })();
-  }, [partnerId]);
   return (
-    <div tw={'flex-auto'}>
-      {messages.map((message) => (
-        <p key={message.id}>
-          <b>
-            {message.author === user?.uid
-              ? 'You'
-              : partnerData?.name || message.author}
-          </b>
-          : {message.text}
-        </p>
-      ))}
-      <input
-        tw={
-          'border border-[#D8D8D8] rounded-[6px] px-[16px] text-[16px] w-36 h-[40px]'
-        }
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyUp={(e) => {
-          if (e.key === 'Enter') {
-            onSend();
-          }
-        }}
-      />
-      <button
-        tw="mt-12 h-9 w-20 relative -top-0.5 text-white bg-theme-red rounded-[6px] px-4 py-1 cursor-pointer hover:bg-[#be4040]"
-        type={'button'}
-        onClick={onSend}
-      >
-        Send
-      </button>
+    <div tw="flex-grow-[2] px-7 flex flex-col relative">
+      <div tw="pt-7 pb-5 border-b border-b-[#D8D8D8] flex items-center">
+        <div tw="ml-2 w-[52px] h-[52px] relative overflow-hidden rounded-full">
+          <Image src={pfp} alt="pfp" layout="fill" objectFit="cover" />
+        </div>
+        <div tw="ml-4">
+          <div tw="text-[20px] font-semibold text-black leading-[1em]">
+            {name}
+          </div>
+          {/*<div tw="text-[14px] text-[#838383] leading-[1em] mt-2">Online</div>*/}
+        </div>
+      </div>
+      <div tw="flex flex-col-reverse overflow-auto pr-1" ref={scrollRef}>
+        <div>
+          {messages.map((msg, i, msgs) => {
+            const showTime =
+              i === 0 || msg.timestamp - msgs[i - 1].timestamp > 1000 * 60 * 15;
+            const isCurrentUser = msg.author === user?.uid;
+            return (
+              <div key={i}>
+                {showTime && (
+                  <div tw="flex justify-center mt-4 text-[12px] text-[#838383]">
+                    {parseDate(msg.timestamp)}
+                  </div>
+                )}
+                <div
+                  tw="flex items-end"
+                  css={[
+                    isCurrentUser ? tw`justify-end` : tw`justify-start`,
+                    i > 0 &&
+                    isCurrentUser === (msgs[i - 1].author === user?.uid) &&
+                    !showTime
+                      ? tw`mt-[2px]`
+                      : tw`mt-4`,
+                  ]}
+                >
+                  {!isCurrentUser &&
+                  (i + 1 >= msgs.length || msgs[i + 1].author === user?.uid) ? (
+                    <div tw="w-9 h-9 mr-3 rounded-full relative overflow-hidden">
+                      <Image
+                        src={pfp}
+                        alt="other pfp"
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                    </div>
+                  ) : (
+                    <div tw="w-12" />
+                  )}
+                  <div
+                    tw="max-w-[75%] py-2 px-4 rounded-[20px]"
+                    css={[
+                      isCurrentUser
+                        ? tw`bg-[#F4F4F4]`
+                        : tw`border border-[#D8D8D8] bg-white`,
+                      i > 0 &&
+                        isCurrentUser &&
+                        msgs[i - 1].author === user?.uid &&
+                        tw`rounded-tr-[6px]`,
+                      i > 0 &&
+                        !isCurrentUser &&
+                        !(msgs[i - 1].author === user?.uid) &&
+                        tw`rounded-tl-[6px]`,
+                      i + 1 < msgs.length &&
+                        isCurrentUser &&
+                        msgs[i + 1].author === user?.uid &&
+                        tw`rounded-br-[6px]`,
+                      i + 1 < msgs.length &&
+                        !isCurrentUser &&
+                        !(msgs[i + 1].author === user?.uid) &&
+                        tw`rounded-bl-[6px]`,
+                    ]}
+                  >
+                    {msg.text}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div tw="mt-auto py-4 bg-white">
+        <div tw="flex items-center rounded-[25px] bg-[#F4F4F4] h-11 px-6">
+          <input
+            ref={messageRef}
+            tw="w-full bg-transparent outline-none text-[14px]"
+            placeholder="Message..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                onSend();
+              }
+            }}
+          />
+          {/* TODO: change to upload file */}
+          <button tw="flex items-center" onClick={() => onSend()}>
+            <Image src="/assets/svgs/send.svg" width="18px" height="18px" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
