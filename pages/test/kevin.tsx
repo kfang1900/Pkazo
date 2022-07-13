8b40c23a2ef2da514c5b7c8b36d36a6225dc7061
Kevin Fang <94582881+kfang1900@users.noreply.github.com>
2022-07-13 01:03:42 -0400

    asdfasdf

--- pages/test/kevin.tsx
+++ pages/test/kevin.tsx
@@ -4,7 +4,7 @@ const GFP = () => {
     return <div tw=''>
         <div tw='flex'>
             {
-                ['a', 'b', 'c', 'd', 'e'].map((x, i) => {
+                ['a', 'bob', 'c', 'd', 'e'].map((x, i) => {
                     return <div key={x} tw='w-10 h-10 border border-black rounded-[10px] text-center'>
                         {x}, {i}
                     </div>
