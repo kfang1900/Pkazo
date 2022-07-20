f5492ac6598671ae92731cc4831170c5f4ed3537
wolfy26 <43702412+wolfy26@users.noreply.github.com>
2022-07-20 05:57:21 -0400

    fixed individual store formatting

--- components/profile/StorePortfolio.tsx
+++ components/profile/StorePortfolio.tsx
@@ -311,214 +311,209 @@ const StorePortFolio = ({
   };
 
   return (
-    <>
-      <h1>Store</h1>
-      <>
-        <div>
-          <Container tw="px-4 md:px-0">
-            {profileType === 3 && (
-              <div tw="flex justify-center gap-6 md:gap-[140px] mt-4 md:mt-12">
-                {portfolioData.Portfolios.map((portfolio, index) => (
-                  <div
-                    key={index}
-                    tw="cursor-pointer"
-                    onClick={() => {
-                      setActiveIndex(activeIndex === index ? null : index);
-                      // setCurGallery(
-                      //   getGalleryData(
-                      //     portfolioData,
-                      //     activeIndex === index ? null : index
-                      //   )
-                      // );
-                    }}
-                  >
-                    <div
-                      css={[
-                        tw`relative rounded-full overflow-hidden origin-bottom border-transparent`,
-                        activeIndex === index && tw`border-[#C6C5C3]`,
-                        isMobile
-                          ? tw`w-[60px] h-[60px] border-2`
-                          : tw`w-[128px] h-[128px] border-4`,
-                      ]}
-                    >
-                      {portfolioData.PortfolioImages[index] && (
-                        <Image
-                          src={portfolioData.PortfolioImages[index]}
-                          alt="Portfolio Image"
-                          layout="fill"
-                        />
-                      )}
-                    </div>
-
-                    <div
-                      css={[
-                        tw`text-[#3C3C3C] text-center`,
-                        isMobile ? tw`text-[12px] mt-1` : tw`text-[16px] mt-2`,
-                      ]}
-                    >
-                      {portfolio.name}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
-            {/* Heading */}
-            <div
-              tw="flex items-center gap-x-3 md:gap-x-6 w-full"
-              css={[
-                profileType === 1 && tw`mt-3 md:mt-6`,
-                profileType === 2 && tw`mt-4 md:mt-[52px]`,
-                profileType === 3 && tw`mt-4 md:mt-12`,
-              ]}
-            >
-              {/* Filter Button */}
+    <div>
+      <Container tw="px-4 md:px-0">
+        {profileType === 3 && (
+          <div tw="flex justify-center gap-6 md:gap-[140px] mt-4 md:mt-12">
+            {portfolioData.Portfolios.map((portfolio, index) => (
               <div
-                onClick={() => setFilterOpen(true)}
-                tw="cursor-pointer flex items-center border border-[#D8D8D8] focus:border-[#A2A2A2] text-[#65676B]"
-                css={[
-                  isMobile
-                    ? tw`w-[32px] h-[32px] rounded-full justify-center`
-                    : tw`gap-[10px] h-11 pl-[22px] pr-6 rounded-[40px]`,
-                ]}
+                key={index}
+                tw="cursor-pointer"
+                onClick={() => {
+                  setActiveIndex(activeIndex === index ? null : index);
+                  // setCurGallery(
+                  //   getGalleryData(
+                  //     portfolioData,
+                  //     activeIndex === index ? null : index
+                  //   )
+                  // );
+                }}
               >
-                <img
-                  src="/assets/svgs/filter.svg"
-                  tw="w-4 h-4 md:w-auto md:h-auto"
-                />
-                {!isMobile && 'All Filters'}
-              </div>
-              {/* Search  */}
-              {/*<CustomSearch />*/}
-              {/* Portfolio for profile 1 and 2 */}
-              {profileType !== 3 && !isMobile && (
-                <Dropdown
-                  onChange={(event) => {
-                    getPortfolioMatch(event.target.value);
-                  }}
-                  appearance={tw`border-[#D8D8D8] rounded-[40px] pl-5 min-w-[160px] h-11 focus:border-[#A2A2A2] text-[#3C3C3C]`}
+                <div
+                  css={[
+                    tw`relative rounded-full overflow-hidden origin-bottom border-transparent`,
+                    activeIndex === index && tw`border-[#C6C5C3]`,
+                    isMobile
+                      ? tw`w-[60px] h-[60px] border-2`
+                      : tw`w-[128px] h-[128px] border-4`,
+                  ]}
                 >
-                  <option value="all">
-                    {profileType === 1 ? 'All Portfolios' : 'Category'}
-                  </option>
-                  {portfolioData.Portfolios.map((portfolio) => (
-                    <option value={portfolio.name} key={portfolio.name}>
-                      {portfolio.name}
-                    </option>
-                  ))}
-                </Dropdown>
-              )}
-              {/* Price Sort */}
-              {/* <CustomSortBy /> */}
-              {/*/!* Grid *!/*/}
-              {/*<div tw="flex items-center text-2xl rounded border border-gray-200">*/}
-              {/*  <span tw="px-4 py-2 cursor-pointer transition-all duration-300 text-gray-700 hover:text-black border-r border-gray-200">*/}
-              {/*    <MdWidgets />*/}
-              {/*  </span>*/}
-              {/*  <span tw="px-4 py-2 cursor-pointer transition-all duration-300 text-gray-700 hover:text-black">*/}
-              {/*    <BsGrid3X3GapFill />*/}
-              {/*  </span>*/}
-              {/*</div>*/}
-            </div>
-
-            {/* No Portfolio Found */}
-            {!loading && works.length === 0 && (
-              <div tw="text-[#3C3C3C] text-[12px] md:text-[16px] mt-4 md:mt-8">
-                No works for sale
-              </div>
-            )}
-
-            {/* Portfolio Gallery */}
-            <div tw="mb-4 md:mb-[30px]">
-              {!loading && <ProductCollection {...works} />}
-            </div>
-          </Container>
-
-          <div>
-            <div
-              css={[
-                tw`fixed bg-white max-w-full z-[99] transition-all duration-300`,
-                isMobile ?
-                  (filterOpen ? tw`bottom-0` : tw`-bottom-full`) :
-                  (filterOpen ? tw`left-0` : tw`-left-full`),
-                isMobile ? tw`left-0 right-0` : tw`top-0 bottom-0`
-              ]}
-            >
-              {filterOpen && <style>{`body {overflow: hidden}`}</style>}
-              {!isMobile &&
-                <span
-                  onClick={() => setFilterOpen(false)}
-                  className="close-icon"
-                  tw="before:content-[''] before:w-0 before:transition-all before:duration-300 hover:before:w-full hover:before:h-full before:h-0 before:absolute before:bg-white/20 before:rounded-full before:z-[-1] absolute text-white cursor-pointer rounded-full flex items-center justify-center text-3xl w-[50px] h-[50px] top-5 right-[-60px]"
-                >
-                  <FiX />
-                </span>
-              }
-
-              {/* Filter Box */}
-              <div tw='w-full md:w-[400px] h-[100vh] overflow-auto'>
-                <div tw='p-8'>
-                  <div tw='flex w-full items-center justify-between'>
-                    <div tw='text-[32px] text-[#3C3C3C]'>Filters</div>
-                    {isMobile &&
-                      <button tw="w-5 h-5" onClick={() => setFilterOpen(false)}>
-                        <svg
-                          width="20"
-                          height="20"
-                          viewBox="0 0 16 16"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M9.40898 7.99829L15.7044 1.71285C15.8926 1.52468 15.9983 1.26947 15.9983 1.00336C15.9983 0.737256 15.8926 0.482046 15.7044 0.293879C15.5162 0.105711 15.261 0 14.9949 0C14.7288 0 14.4736 0.105711 14.2854 0.293879L8 6.58932L1.71456 0.293879C1.52639 0.105711 1.27118 2.36263e-07 1.00507 2.38246e-07C0.738961 2.40229e-07 0.483751 0.105711 0.295584 0.293879C0.107417 0.482046 0.0017056 0.737256 0.0017056 1.00336C0.0017056 1.26947 0.107417 1.52468 0.295584 1.71285L6.59102 7.99829L0.295584 14.2837C0.201924 14.3766 0.127583 14.4872 0.0768515 14.6089C0.0261196 14.7307 0 14.8613 0 14.9932C0 15.1251 0.0261196 15.2558 0.0768515 15.3775C0.127583 15.4993 0.201924 15.6098 0.295584 15.7027C0.38848 15.7964 0.499001 15.8707 0.620772 15.9214C0.742543 15.9722 0.873154 15.9983 1.00507 15.9983C1.13699 15.9983 1.2676 15.9722 1.38937 15.9214C1.51114 15.8707 1.62166 15.7964 1.71456 15.7027L8 9.40727L14.2854 15.7027C14.3783 15.7964 14.4889 15.8707 14.6106 15.9214C14.7324 15.9722 14.863 15.9983 14.9949 15.9983C15.1268 15.9983 15.2575 15.9722 15.3792 15.9214C15.501 15.8707 15.6115 15.7964 15.7044 15.7027C15.7981 15.6098 15.8724 15.4993 15.9232 15.3775C15.9739 15.2558 16 15.1251 16 14.9932C16 14.8613 15.9739 14.7307 15.9232 14.6089C15.8724 14.4872 15.7981 14.3766 15.7044 14.2837L9.40898 7.99829Z"
-                            fill="#3C3C3C"
-                          />
-                        </svg>
-                      </button>
-                    }
-                  </div>
-                  <div tw='mt-6 flex flex-col gap-y-7'>
-                    <FilterCategory {...filters['portfolio']} />
-                    <FilterCategory {...filters['price']} isPrice />
-                    <FilterCategory {...filters['size']} />
-                    <FilterCategory {...filters['medium']} />
-                    <FilterCategory {...filters['surface']} />
-                    <FilterCategory {...filters['subject']} />
-                    <FilterCategory {...filters['style']} />
-                  </div>
+                  {portfolioData.PortfolioImages[index] && (
+                    <Image
+                      src={portfolioData.PortfolioImages[index]}
+                      alt="Portfolio Image"
+                      layout="fill"
+                    />
+                  )}
                 </div>
-                <div tw="sticky w-full bottom-0 bg-white">
-                  <div tw='h-[0.5px] bg-[#E3E3E3]' />
-                  <div tw='px-4 flex w-full px-8 py-5 gap-x-3'>
-                    <button
-                      onClick={() => setFilterOpen(false)}
-                      css={[buttons.white, tw`w-full h-10`]}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      onClick={() => setFilterOpen(false)}
-                      css={[buttons.red, tw`w-full h-10`]}
-                    >
-                      Apply
-                    </button>
-                  </div>
+
+                <div
+                  css={[
+                    tw`text-[#3C3C3C] text-center`,
+                    isMobile ? tw`text-[12px] mt-1` : tw`text-[16px] mt-2`,
+                  ]}
+                >
+                  {portfolio.name}
                 </div>
               </div>
-            </div>
-            {!isMobile &&
-              <span
-                onClick={() => setFilterOpen(false)}
-                css={[
-                  tw`fixed z-[98] left-0 top-0 bottom-0 bg-[rgba(34,34,34,0.65)]`,
-                  filterOpen && tw`w-full`,
-                ]}
-              ></span>
-            }
+            ))}
           </div>
-          {/* <DrawerFilter drawerToggle={drawerToggle} handleCloseFilter={handleCloseFilter}/> */}
+        )}
+        {/* Heading */}
+        <div
+          tw="flex items-center gap-x-3 md:gap-x-6 w-full"
+          css={[
+            profileType === 1 && tw`mt-3 md:mt-6`,
+            profileType === 2 && tw`mt-4 md:mt-[52px]`,
+            profileType === 3 && tw`mt-4 md:mt-12`,
+          ]}
+        >
+          {/* Filter Button */}
+          <div
+            onClick={() => setFilterOpen(true)}
+            tw="cursor-pointer flex items-center border border-[#D8D8D8] focus:border-[#A2A2A2] text-[#65676B]"
+            css={[
+              isMobile
+                ? tw`w-[32px] h-[32px] rounded-full justify-center`
+                : tw`gap-[10px] h-11 pl-[22px] pr-6 rounded-[40px]`,
+            ]}
+          >
+            <img
+              src="/assets/svgs/filter.svg"
+              tw="w-4 h-4 md:w-auto md:h-auto"
+            />
+            {!isMobile && 'All Filters'}
+          </div>
+          {/* Search  */}
+          {/*<CustomSearch />*/}
+          {/* Portfolio for profile 1 and 2 */}
+          {profileType !== 3 && !isMobile && (
+            <Dropdown
+              onChange={(event) => {
+                getPortfolioMatch(event.target.value);
+              }}
+              appearance={tw`border-[#D8D8D8] rounded-[40px] pl-5 min-w-[160px] h-11 focus:border-[#A2A2A2] text-[#3C3C3C]`}
+            >
+              <option value="all">
+                {profileType === 1 ? 'All Portfolios' : 'Category'}
+              </option>
+              {portfolioData.Portfolios.map((portfolio) => (
+                <option value={portfolio.name} key={portfolio.name}>
+                  {portfolio.name}
+                </option>
+              ))}
+            </Dropdown>
+          )}
+          {/* Price Sort */}
+          {/* <CustomSortBy /> */}
+          {/*/!* Grid *!/*/}
+          {/*<div tw="flex items-center text-2xl rounded border border-gray-200">*/}
+          {/*  <span tw="px-4 py-2 cursor-pointer transition-all duration-300 text-gray-700 hover:text-black border-r border-gray-200">*/}
+          {/*    <MdWidgets />*/}
+          {/*  </span>*/}
+          {/*  <span tw="px-4 py-2 cursor-pointer transition-all duration-300 text-gray-700 hover:text-black">*/}
+          {/*    <BsGrid3X3GapFill />*/}
+          {/*  </span>*/}
+          {/*</div>*/}
         </div>
-      </>
-    </>
+
+        {/* No Portfolio Found */}
+        {!loading && works.length === 0 && (
+          <div tw="text-[#3C3C3C] text-[12px] md:text-[16px] mt-4 md:mt-8">
+            No works for sale
+          </div>
+        )}
+
+        {/* Portfolio Gallery */}
+        <div tw="mb-4 md:mb-[30px]">
+          {!loading && <ProductCollection {...works} />}
+        </div>
+      </Container>
+
+      <div>
+        <div
+          css={[
+            tw`fixed bg-white max-w-full z-[99] transition-all duration-300`,
+            isMobile ?
+              (filterOpen ? tw`bottom-0` : tw`-bottom-full`) :
+              (filterOpen ? tw`left-0` : tw`-left-full`),
+            isMobile ? tw`left-0 right-0` : tw`top-0 bottom-0`
+          ]}
+        >
+          {filterOpen && <style>{`body {overflow: hidden}`}</style>}
+          {!isMobile &&
+            <span
+              onClick={() => setFilterOpen(false)}
+              className="close-icon"
+              tw="before:content-[''] before:w-0 before:transition-all before:duration-300 hover:before:w-full hover:before:h-full before:h-0 before:absolute before:bg-white/20 before:rounded-full before:z-[-1] absolute text-white cursor-pointer rounded-full flex items-center justify-center text-3xl w-[50px] h-[50px] top-5 right-[-60px]"
+            >
+              <FiX />
+            </span>
+          }
+
+          {/* Filter Box */}
+          <div tw='w-full md:w-[400px] h-[100vh] overflow-auto'>
+            <div tw='p-8'>
+              <div tw='flex w-full items-center justify-between'>
+                <div tw='text-[32px] text-[#3C3C3C]'>Filters</div>
+                {isMobile &&
+                  <button tw="w-5 h-5" onClick={() => setFilterOpen(false)}>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9.40898 7.99829L15.7044 1.71285C15.8926 1.52468 15.9983 1.26947 15.9983 1.00336C15.9983 0.737256 15.8926 0.482046 15.7044 0.293879C15.5162 0.105711 15.261 0 14.9949 0C14.7288 0 14.4736 0.105711 14.2854 0.293879L8 6.58932L1.71456 0.293879C1.52639 0.105711 1.27118 2.36263e-07 1.00507 2.38246e-07C0.738961 2.40229e-07 0.483751 0.105711 0.295584 0.293879C0.107417 0.482046 0.0017056 0.737256 0.0017056 1.00336C0.0017056 1.26947 0.107417 1.52468 0.295584 1.71285L6.59102 7.99829L0.295584 14.2837C0.201924 14.3766 0.127583 14.4872 0.0768515 14.6089C0.0261196 14.7307 0 14.8613 0 14.9932C0 15.1251 0.0261196 15.2558 0.0768515 15.3775C0.127583 15.4993 0.201924 15.6098 0.295584 15.7027C0.38848 15.7964 0.499001 15.8707 0.620772 15.9214C0.742543 15.9722 0.873154 15.9983 1.00507 15.9983C1.13699 15.9983 1.2676 15.9722 1.38937 15.9214C1.51114 15.8707 1.62166 15.7964 1.71456 15.7027L8 9.40727L14.2854 15.7027C14.3783 15.7964 14.4889 15.8707 14.6106 15.9214C14.7324 15.9722 14.863 15.9983 14.9949 15.9983C15.1268 15.9983 15.2575 15.9722 15.3792 15.9214C15.501 15.8707 15.6115 15.7964 15.7044 15.7027C15.7981 15.6098 15.8724 15.4993 15.9232 15.3775C15.9739 15.2558 16 15.1251 16 14.9932C16 14.8613 15.9739 14.7307 15.9232 14.6089C15.8724 14.4872 15.7981 14.3766 15.7044 14.2837L9.40898 7.99829Z"
+                        fill="#3C3C3C"
+                      />
+                    </svg>
+                  </button>
+                }
+              </div>
+              <div tw='mt-6 flex flex-col gap-y-7'>
+                <FilterCategory {...filters['portfolio']} />
+                <FilterCategory {...filters['price']} isPrice />
+                <FilterCategory {...filters['size']} />
+                <FilterCategory {...filters['medium']} />
+                <FilterCategory {...filters['surface']} />
+                <FilterCategory {...filters['subject']} />
+                <FilterCategory {...filters['style']} />
+              </div>
+            </div>
+            <div tw="sticky w-full bottom-0 bg-white">
+              <div tw='h-[0.5px] bg-[#E3E3E3]' />
+              <div tw='px-4 flex w-full px-8 py-5 gap-x-3'>
+                <button
+                  onClick={() => setFilterOpen(false)}
+                  css={[buttons.white, tw`w-full h-10`]}
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => setFilterOpen(false)}
+                  css={[buttons.red, tw`w-full h-10`]}
+                >
+                  Apply
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {!isMobile &&
+          <span
+            onClick={() => setFilterOpen(false)}
+            css={[
+              tw`fixed z-[98] left-0 top-0 bottom-0 bg-[rgba(34,34,34,0.65)]`,
+              filterOpen && tw`w-full`,
+            ]}
+          ></span>
+        }
+      </div>
+      {/* <DrawerFilter drawerToggle={drawerToggle} handleCloseFilter={handleCloseFilter}/> */}
+    </div>
   );
 };
 /*
--- components/profile/store/ProductCollection.tsx
+++ components/profile/store/ProductCollection.tsx
@@ -5,6 +5,9 @@ import React, { useEffect, useReducer } from 'react';
 import { ForSaleWorkRecord, WorkRecord } from '../../../types/dbTypes';
 import { loadStorageImageSafe } from '../../../helpers/FirebaseFunctions';
 import { WorkData } from '../../../types/dbTypes';
+import { useMediaQuery } from 'react-responsive';
+import Link from 'next/link';
+import 'twin.macro';
 
 interface WorkPackage {
   id: string;
@@ -16,27 +19,72 @@ interface WorkPackage {
 export default function ProductCollection(props: WorkPackage[]) {
   const workprops: WorkPackage[] = Object.values(props);
   console.log(workprops);
+
+  const isMobile = !useMediaQuery({ query: `(min-width: 768px)` });
+
   return (
     <Masonry
       breakpointCols={{ default: 3, 500: 2 }}
       className={styles['masonry-column']}
       columnClassName={styles['store-column']}
-      tw="mt-100"
     >
       {/*/!* Single Portfolio *!/*/}
       {workprops.map(({ id, data: work, imageURL }, i) => (
-        <a key={id} tw={'cursor-pointer my-[18px]'} href={'/work/' + id}>
-          <div tw="w-full mb-6 mt-4">
-            <img src={imageURL} alt="Image Alt" />
+        <Link key={i} href={'/work/' + id} passHref>
+          <div tw="cursor-pointer my-4 md:my-10">
+            {imageURL && <img src={imageURL} alt="Image Alt" tw="w-full" />}
+            {isMobile ? (
+              <div tw="mt-2 flex justify-between">
+                <div>
+                  <div tw="text-[#65676B] text-[12px] leading-[1em] font-semibold">
+                    {work.title}
+                  </div>
+                  <div tw="text-[#838383] text-[9px] leading-[1em] italic mt-[6px]">
+                    {work.medium}
+                  </div>
+                </div>
+                <div tw="text-black text-[14px] leading-[1em] font-bold">
+                  ${work.sale ? work.sale.price : 'NFS!!'}
+                </div>
+              </div>
+            ) : (
+              <div tw="mt-4">
+                <div tw="text-[#3C3C3C] text-[20px] leading-[1em] font-semibold">
+                  {work.title}{' '}
+                  {/* <button
+                    tw={'underline italic ml-8'}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setEditing(true);
+                    }}
+                  >
+                    Edit
+                  </button> */}
+                </div>
+                <div tw="flex justify-between mt-2">
+                  <div tw="text-[#838383] text-[18px] leading-[1em] italic">
+                    {work.medium}
+                  </div>
+                  <div tw="text-black text-[24px] leading-[1em] font-bold">
+                    ${work.sale ? work.sale.price : 'NFS!!'}
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-          <div>
-            <h4>{work.title}</h4>
-            <div tw="flex items-center justify-between">
-              <p tw="text-gray-600">{work.medium}</p>
-              <strong>${work.sale.price}</strong>
-            </div>
-          </div>
-        </a>
+        </Link>
+        // <a key={id} tw={'cursor-pointer my-[18px]'} href={'/work/' + id}>
+        //   <div tw="w-full mb-6 mt-4">
+        //     <img src={imageURL} alt="Image Alt" />
+        //   </div>
+        //   <div>
+        //     <h4>{work.title}</h4>
+        //     <div tw="flex items-center justify-between">
+        //       <p tw="text-gray-600">{work.medium}</p>
+        //       <strong>${work.sale.price}</strong>
+        //     </div>
+        //   </div>
+        // </a>
       ))}
     </Masonry>
   );
