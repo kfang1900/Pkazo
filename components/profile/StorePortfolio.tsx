938c9ae857bdcf6909c6651564b560bb6e2c4014
wolfy26 <43702412+wolfy26@users.noreply.github.com>
2022-07-27 00:47:55 -0400

    test 3

--- components/profile/StorePortfolio.tsx
+++ components/profile/StorePortfolio.tsx
@@ -457,7 +457,7 @@ const StorePortFolio = ({
           }
 
           {/* Filter Box */}
-          <div tw='w-full md:w-[400px] overflow-auto'>
+          <div tw='w-full md:w-[400px] max-h-[100vh] overflow-auto'>
             <div tw='p-8'>
               <div tw='flex w-full items-center justify-between'>
                 <div tw='text-[32px] text-[#3C3C3C]'>Filters</div>
